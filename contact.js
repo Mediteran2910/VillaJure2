8b6c78d0a28b4b53571deacaa46d9a4814d38a3f
Mediteran2910 <marindonadini00@gmail.com>
2024-12-02 01:02:29 +0100

    making contact page responsive

--- contact.js
+++ contact.js
@@ -50,3 +50,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
   priceWrappers.forEach((wrapper) => observer.observe(wrapper));
 });
+
+const menuIcon = document.getElementById("menu-icon");
+const closeMenuIcon = document.querySelector(".close-icon-menu");
+const mobileMenu = document.getElementsByClassName("nav-links")[0];
+
+menuIcon.addEventListener("click", () => {
+  mobileMenu.style.display = "flex";
+});
+
+closeMenuIcon.addEventListener("click", () => {
+  mobileMenu.style.display = "none";
+});
+
+function changeH1size() {
+  const h1 = document.querySelector("#main-title");
+  const titleParagraph = document.getElementById("paragraph-title");
+
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth < 560) {
+    h1.textContent = "Escape Awaits";
+    titleParagraph.textContent = "Where luxury meets ease.";
+  } else if (screenWidth < 730) {
+    h1.textContent = "Your Paradise Awaits";
+    // titleParagraph.textContent = "Villa between sea and hills.";
+  } else {
+    h1.textContent = "Reserve Your Slice of Paradise";
+    titleParagraph.textContent = "Where luxury meets effortless relaxation.";
+  }
+}
+window.addEventListener("resize", changeH1size);
+changeH1size();
